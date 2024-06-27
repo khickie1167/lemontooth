4d8f2554749af3c9bbe82db1e9261c241aca1512
khickie1167 <khickie1167@googlemail.com>
2024-06-27 22:39:49 +0100

    Fourth Commit

--- js/app.js
+++ js/app.js
@@ -1,42 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   const menuButton = document.getElementById('menu-button');
   const subnav = document.getElementById('subnav');
+  const stats = document.querySelector('.stats');
+
+  // Ensure subnav is initially hidden
+  subnav.style.display = 'none';
+  // Set initial margin for stats
+  stats.style.marginTop = '88px'; // Navbar height (64px) + Margin (24px)
 
   menuButton.addEventListener('click', function (e) {
     e.preventDefault();
     if (subnav.style.display === 'none' || subnav.style.display === '') {
       subnav.style.display = 'block';
+      stats.style.marginTop = '24px'; // Navbar height (64px) + Subnav height (64px) + Margin (24px)
     } else {
       subnav.style.display = 'none';
+      stats.style.marginTop = '88px'; // Navbar height (64px) + Margin (24px)
     }
   });
 });
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
